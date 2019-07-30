9d1c835eeb228208c7c978a3ce446c0794af8c27
Kate Storey-Fisher <kstoreyfisher@gmail.com>
2019-07-30 17:11:25 -0400

    attempt at tensorflow-hub

---
+++ py36.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo Setting up python3.6 tensorflow environment
+#module load cudnn/9.0v7.0.5
+module load cudnn/9.0v7.3.0.29 
+module load cuda/9.0.176
+source ~/py36/bin/activate
+#source $HOME/py36/bin/activate
