24839319185a1e4f5b7c98c4f07fae920cc926c6
Kate Storey-Fisher <kstoreyfisher@gmail.com>
2019-07-31 20:21:10 -0400

    added discriminator feature matching to anomaly detection

--- gans/anomaly_detection.py
+++ gans/anomaly_detection.py
@@ -28,13 +28,20 @@ import tflib.plot
 import tflib.datautils
 
 
+
 tag = 'i20.0_norm'
 imarr_fn = f'/scratch/ksf293/kavli/anomaly/data/images_np/imarr_{tag}.npy'
-savetag = '_try3'
+savetag = '_featcpu'
+mode = 'detector'
+#mode = 'simple'
 
 gentag = 'i20.0_norm_hub'
 gennum = 7000
 gen_fn = f'/home/ksf293/kavli/anomalies-GAN-HSC/training_output/out_{gentag}/model-gen-{gennum}'
+
+disctag = 'i20.0_norm_feat'
+discnum = 600
+disc_fn = f'/home/ksf293/kavli/anomalies-GAN-HSC/training_output/out_{disctag}/model-disc-{discnum}'
 
 results_fn = f'/home/ksf293/kavli/anomalies-GAN-HSC/results/results_{tag}{savetag}.npy'
 
@@ -44,8 +51,9 @@ OUTPUT_DIM = NSIDE*NSIDE
 
 print(f"Running anomaly detection for {tag} with generator {gentag}")
 
-print("Loading generator")
+print("Loading generator and discriminator")
 Generator = hub.Module(gen_fn)
+Discriminator = hub.Module(disc_fn)
 print("Loaded")
 
 def Detector(z):
@@ -57,47 +65,64 @@ def Detector(z):
 print("Setting up model")
 real = tf.placeholder(tf.float32, shape=[1, OUTPUT_DIM])
 
-noise = tf.placeholder(tf.float32, shape=[1, 128])
+if mode=='simple':
+    noise = tf.get_variable('Detector', shape=[1, 128], dtype = tf.float32, 
+            initializer = tf.random_normal_initializer())
+    reconstructed = Generator(noise)
 
-#ano_z = tf.get_variable('ano_z', shape=[1, 128], dtype = tf.float32, 
-#        initializer = tf.random_normal_initializer())
+if mode=='detector':
+    noise = tf.placeholder(tf.float32, shape=[1, 128])
+    reconstructed = Generator(Detector(noise)) # 1 sample
 
-#reconstructed = Generator(ano_z)
-reconstructed = Generator(Detector(noise)) # 1 sample
+feature_residual = tf.reduce_sum(tf.abs(tf.subtract(
+                    Discriminator(real, signature='feature_match'),
+                    Discriminator(reconstructed, signature='feature_match')
+                    ))) 
+
 residual = tf.reduce_sum(tf.abs(tf.subtract(real, reconstructed)))
 
+anomaly_weight = 0.1
+anomaly_score = (1-anomaly_weight)*residual + anomaly_weight*feature_residual
+
 t_vars = tf.trainable_variables()
-#params = [var for var in t_vars if 'ano_z' in var.name]
 params = [var for var in t_vars if 'Detector' in var.name]
 
 optimizer = tf.train.AdamOptimizer(
         learning_rate=1e-3, #1e-4,
-        beta1=0.5#,
-        #beta2=0.9
-    ).minimize(residual, var_list=params)
+        beta1=0.4,
+        beta2=0.9
+    ).minimize(anomaly_score, var_list=params)
 
+print("Loading data")
 data = lib.datautils.load_numpy(imarr_fn)
 data_gen = lib.datautils.DataGenerator(data)
 
+data_idxs = [8227, 23384, 27990, 51660, 53654, 75000, 77166, 79478, 136646, 154749, 168076, 169037, 205029, 222372, 230340, 233239, 306282, 371503, 391733, 393979, 403224, 430453, 458481, 537727, 544052, 595328, 646272, 696868, 716897, 786702, 826764, 837307]
+data = data[data_idxs]
+print(f'Num to detect: {len(data)}')
 results = []
 with tf.Session() as sess:
     
     sess.run(tf.global_variables_initializer())
-
+    start = time.time()
     for i in range(len(data)): 
 
-        image, _ = data_gen.next_one()
+        #_image, _ = data_gen.next_one()
+        _image = data[i]
         _noise = np.random.normal(size=(1, 128)).astype('float32')
         
-        for j in range(200):
-            #_residual, _reconstructed, _ano_z, _ = sess.run([residual, reconstructed, ano_z, optimizer],
-            _residual, _reconstructed, _ = sess.run([residual, reconstructed, optimizer],
-                feed_dict={real: image.reshape(1, OUTPUT_DIM)#})
-                                    , noise: _noise})
-        results.append([image, _reconstructed, _residual])
-        #print(_ano_z[0][:10])
-        if (i % 100 == 0) and i>0:
+        for j in range(100):
+            if mode=='simple':
+                feed_dict = {real: _image.reshape(1, OUTPUT_DIM)}
+            if mode=='detector':
+                feed_dict={real: _image.reshape(1, OUTPUT_DIM), noise: _noise}
+            _residual, _feature_residual, _score, _reconstructed, _ = sess.run([residual, feature_residual, anomaly_score, reconstructed, optimizer],
+                                                    feed_dict=feed_dict)
+
+        results.append([_image, _reconstructed, _residual, _feature_residual, _score, data_idxs[i]])
+        if (i==len(data)-1) and i>0:
             print(i, _residual)
             np.save(results_fn, np.array(results))
-    
+            end = time.time()
+            print(f"Time: {end-start} s")
 
