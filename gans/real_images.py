09c089fbbd267a29137a9fa72999bc92e6226658
Kate Storey-Fisher <kstoreyfisher@gmail.com>
2019-08-05 03:33:40 -0400

    fixed real_image typos

--- gans/real_images.py
+++ gans/real_images.py
@@ -1,3 +1,9 @@
+# ******************************************************
+# * File Name : real_images.py
+# * Creation Date : 2019-08-02
+# * Created By : kstoreyf
+# * Description : Generates image thumbnails or batches.
+# ******************************************************
 import numpy as np
 from imageio import imwrite
 
@@ -5,19 +11,22 @@ import tflib.save_images as saver
 
 
 def main():
-    tag = 'i20.0'
-    idx = 15850
-    #imarr96 = np.load(f'/scratch/ksf293/kavli/anomaly/data/images_np/imarr_{tag}.npy')
-    tag = 'i20.0_norm_100k'
-    savetag = f'_{idx}'
+    anoms = [102009] #i20.0_norm
+    #idx = 15850
+    tag = 'i20.0_norm'
     imarr_fn = f'/scratch/ksf293/kavli/anomaly/data/images_np/imarr_{tag}.npy'
-    save_fn = f"../thumbnails/real_{tag}{savetag}.png"
+    #savetag = ''
+    #save_fn = f"../thumbnails/real_{tag}{savetag}.png"
     imarr = np.load(imarr_fn)
-    make_thumbnail(imarr, idx, save_fn)
+    make_thumbnail(imarr, anoms, tag)
 
-def make_thumbnail(idx, imarr, savefn):
-    ims = imarr[idx]
-    imwrite(save_fn, ims)
+def make_thumbnail(imarr, idxs, tag):
+    if type(idxs)==int:
+        idxs = [idxs]
+    for idx in idxs:
+        ims = imarr[idx]
+        save_fn = f"../thumbnails/real_{tag}_{idx}.png"
+        imwrite(save_fn, ims)
 
 
 def make_batch(save_fn, n=128):
