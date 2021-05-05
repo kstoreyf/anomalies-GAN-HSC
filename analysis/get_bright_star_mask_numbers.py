ed8e7670e1b44938a38a624fa02518f771314aae
Kate Storey-Fisher <k.sf@nyu.edu>
2021-05-05 19:34:30 -0400

    update bsm script

--- analysis/get_bright_star_mask_numbers.py
+++ analysis/get_bright_star_mask_numbers.py
@@ -2,7 +2,8 @@ from astropy.table import Table
 
 from caterpillar import catalog
 
-base_dir = '/Users/ksf/code/kavli/anomalies-GAN-HSC'
+#base_dir = '/Users/ksf/code/kavli/anomalies-GAN-HSC'
+base_dir = '/scratch/ksf293/anomalies'
 mask_dir = f'{base_dir}/data/hsc_masks/'
 
 anom_file = f'{base_dir}/data/hsc_catalogs/anomaly_catalog_hsc_full.fits'
@@ -12,12 +13,12 @@ anom_msk = catalog.filter_through_bright_star_mask(
     anom_file, mask_dir, filter_type='inside', output_suffix='masked')
 
 n_total = len(anom_cat)
-anoms_total = len(anom_cat['discriminator_score_normalized'] > 3)
+anoms_total = len(anom_cat[anom_cat['discriminator_score_normalized'] > 3])
 print("Total everywhere:", n_total)
 print("Anoms everywhere:", anoms_total)
 
 print("Total inside mask:", len(anom_msk))
 print("% total inside mask:", len(anom_msk)/n_total)
-anoms_inmask = len(anom_msk['discriminator_score_normalized'] > 3)
+anoms_inmask = len(anom_msk[anom_msk['discriminator_score_normalized'] > 3])
 print("Anomalies inside mask:", anoms_inmask)
 print("% Anomalies inside mask:", anoms_inmask/anoms_total)
